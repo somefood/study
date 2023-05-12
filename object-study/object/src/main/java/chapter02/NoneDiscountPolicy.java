aec636bfe2e0942fb811374ac1aca32e647f21a0
Hong SeokJu <10017011@wjtb.net>
2023-05-12 16:57:00 +0900

    오브젝트 2장 Movie

---
+++ object-study/object/src/main/java/chapter02/NoneDiscountPolicy.java
@@ -0,0 +1,9 @@
+package chapter02;
+
+public class NoneDiscountPolicy extends DiscountPolicy {
+
+    @Override
+    protected Money getDiscountAmount(Screening screening) {
+        return Money.ZERO;
+    }
+}
