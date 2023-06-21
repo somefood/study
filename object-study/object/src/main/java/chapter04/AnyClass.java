53dd20a0dd89116890c406c2311474f282d1b7c3
Hong SeokJu <10017011@wjtb.net>
2023-06-21 18:29:43 +0900

    오브젝트 4장 코드
    개선 1 - 자율적인 객체를 향해 (캡슐화를 지켜라)

---
+++ object-study/object/src/main/java/chapter04/AnyClass.java
@@ -0,0 +1,9 @@
+package chapter04;
+
+public class AnyClass {
+
+    void anyMethod(Rectangle rectangle, int multiple) {
+        rectangle.setRight(rectangle.getRight() * multiple);
+        rectangle.setBottom(rectangle.getBottom() * multiple);
+    }
+}
--- object-study/object/src/main/java/chapter04/Movie.java
+++ object-study/object/src/main/java/chapter04/Movie.java
@@ -1,6 +1,7 @@
 package chapter04;
 
 import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class Movie {
@@ -16,59 +17,46 @@ public class Movie {
 
     private double discountPercent;
 
-    public String getTitle() {
-        return title;
-    }
-
-    public Duration getRunningTime() {
-        return runningTime;
-    }
-
-    public Money getFee() {
-        return fee;
-    }
-
-    public List<DiscountCondition> getDiscountConditions() {
-        return discountConditions;
-    }
-
     public MovieType getMovieType() {
         return movieType;
     }
 
-    public Money getDiscountAmount() {
-        return discountAmount;
+    public Money calculateAmountDiscountedFee() {
+        if (movieType != MovieType.AMOUNT_DISCOUNT) {
+            throw new IllegalArgumentException();
+        }
+
+        return fee.minus(discountAmount);
     }
 
-    public double getDiscountPercent() {
-        return discountPercent;
+    public Money calculatePercentDiscountedFee() {
+        if (movieType != MovieType.PERCENT_DISCOUNT) {
+            throw new IllegalArgumentException();
+        }
+
+        return fee.minus(fee.times(discountPercent));
     }
 
-    public void setTitle(String title) {
-        this.title = title;
+    public Money calculateNoneDiscountedFee() {
+        if (movieType != MovieType.NONE_DISCOUNT) {
+            throw new IllegalArgumentException();
+        }
+
+        return fee;
     }
 
-    public void setRunningTime(Duration runningTime) {
-        this.runningTime = runningTime;
-    }
-
-    public void setFee(Money fee) {
-        this.fee = fee;
-    }
-
-    public void setDiscountConditions(List<DiscountCondition> discountConditions) {
-        this.discountConditions = discountConditions;
-    }
-
-    public void setMovieType(MovieType movieType) {
-        this.movieType = movieType;
-    }
-
-    public void setDiscountAmount(Money discountAmount) {
-        this.discountAmount = discountAmount;
-    }
-
-    public void setDiscountPercent(double discountPercent) {
-        this.discountPercent = discountPercent;
+    public boolean isDiscountable(LocalDateTime whenScreened, int sequence) {
+        for (DiscountCondition condition : discountConditions) {
+            if (condition.getType() == DiscountConditionType.PERIOD) {
+                if (condition.isDiscountable(whenScreened.getDayOfWeek(), whenScreened.toLocalTime())) {
+                    return true;
+                }
+            } else {
+                if (condition.isDiscountable(sequence)) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
