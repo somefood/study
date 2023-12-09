5c7f54920abfa4028633a69c67f4b744f325efb5
Seokju Hong <iamseokjuhong@gmail.com>
2023-12-09 14:40:42 +0900

    8장 정리

---
+++ java-start/src/array/Array1Ref1.java
@@ -0,0 +1,22 @@
+package array;
+
+public class Array1Ref1 {
+
+    public static void main(String[] args) {
+        int[] students; // 배열 변수 선언
+        students = new int[5];
+
+        System.out.println(students); // I: int형 배열 의미
+
+        // 변수 값 대입
+        students[0] = 90;
+        students[1] = 80;
+        students[2] = 70;
+        students[3] = 60;
+        students[4] = 40;
+
+        for (int i = 0; i < students.length; i++) {
+            System.out.println("학생" + (i + 1) + " 점수: " + students[i]);
+        }
+    }
+}
---
+++ java-start/src/array/ArrayDi4.java
@@ -0,0 +1,23 @@
+package array;
+
+public class ArrayDi4 {
+
+    public static void main(String[] args) {
+        int[][] arr = new int[5][5];
+
+        int i = 1;
+        for (int row = 0; row < arr.length; row++) {
+            for (int column = 0; column < arr[row].length; column++) {
+                arr[row][column] = i++;
+            }
+        }
+
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr[row].length; col++) {
+                System.out.print(arr[row][col] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
