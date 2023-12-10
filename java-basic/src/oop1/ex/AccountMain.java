a33282815149949c3989a58a3bd3dd301ce3abf0
Seokju Hong <iamseokjuhong@gmail.com>
2023-12-11 01:14:17 +0900

    실전 자바 - 3장 정리

--- java-basic/src/oop1/ex/AccountMain.java
+++ java-basic/src/oop1/ex/AccountMain.java
@@ -3,7 +3,7 @@ package oop1.ex;
 public class AccountMain {
     public static void main(String[] args) {
         Account account = new Account();
-        account.balance = 10000;
+        account.deposit(10000);
 
         account.withdraw(9000);
         account.withdraw(2000);
