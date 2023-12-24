f5444eab991ec4ba23563164ecadc127d0137366
Seokju Hong <iamseokjuhong@gmail.com>
2023-12-24 22:49:43 +0900

    토비 스프링 시작

---
+++ toby-spring/src/test/java/me/somefood/tobyspring/TobySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package me.somefood.tobyspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TobySpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
