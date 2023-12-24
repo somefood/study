b9b50ec147bfd9fe8a2853fbfd43b01002351711
Seokju Hong <iamseokjuhong@gmail.com>
2023-12-25 01:13:40 +0900

    자바코드 구성 정보 사용

--- toby-spring/src/main/java/me/somefood/tobyspring/TobySpringApplication.java
+++ toby-spring/src/main/java/me/somefood/tobyspring/TobySpringApplication.java
@@ -2,14 +2,27 @@ package me.somefood.tobyspring;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
-import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
+@Configuration
 public class TobySpringApplication {
+
+    @Bean
+    public HelloController helloController(HelloService helloService) {
+        return new HelloController(helloService);
+    }
+
+    @Bean
+    public HelloService helloService() {
+        return new SimpleHelloService();
+    }
 
     public static void main(String[] args) {
 
-        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
             @Override
             protected void onRefresh() {
                 super.onRefresh();
@@ -24,8 +37,7 @@ public class TobySpringApplication {
             }
         };
 
-        applicationContext.registerBean(HelloController.class);
-        applicationContext.registerBean(SimpleHelloService.class);
+        applicationContext.register(TobySpringApplication.class);
         applicationContext.refresh(); // 빈 오브젝트 생성
     }
 }
