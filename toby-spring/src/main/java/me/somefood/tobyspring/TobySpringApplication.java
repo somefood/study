09e1d5872debb3e395e9102ca027b867972a94da
Seokju Hong <iamseokjuhong@gmail.com>
2023-12-25 01:08:26 +0900

    스프링 컨테이너로 통합

--- toby-spring/src/main/java/me/somefood/tobyspring/TobySpringApplication.java
+++ toby-spring/src/main/java/me/somefood/tobyspring/TobySpringApplication.java
@@ -9,17 +9,23 @@ public class TobySpringApplication {
 
     public static void main(String[] args) {
 
-        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
+        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+            @Override
+            protected void onRefresh() {
+                super.onRefresh();
+
+                TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+                WebServer webServer = serverFactory.getWebServer(servletContext -> {
+                    servletContext.addServlet("dispatcherServlet",
+                            new DispatcherServlet(this)
+                    ).addMapping("/*");
+                });
+                webServer.start();
+            }
+        };
+
         applicationContext.registerBean(HelloController.class);
         applicationContext.registerBean(SimpleHelloService.class);
         applicationContext.refresh(); // 빈 오브젝트 생성
-
-        TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-        WebServer webServer = serverFactory.getWebServer(servletContext -> {
-            servletContext.addServlet("dispatcherServlet",
-                    new DispatcherServlet(applicationContext)
-            ).addMapping("/*");
-        });
-        webServer.start();
     }
 }
