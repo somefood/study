45fd9df112f9ade2bb4e92935dda342f77bc3283
somefood <iamseokjuhong@gmail.com>
2023-12-26 12:58:57 +0900

    Bean의 생명주기 메소드

--- toby-spring/src/main/java/me/somefood/tobyspring/TobySpringApplication.java
+++ toby-spring/src/main/java/me/somefood/tobyspring/TobySpringApplication.java
@@ -2,6 +2,8 @@ package me.somefood.tobyspring;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
@@ -11,6 +13,16 @@ import org.springframework.web.servlet.DispatcherServlet;
 @ComponentScan
 public class TobySpringApplication {
 
+    @Bean
+    public ServletWebServerFactory servletWebServerFactory() {
+        return new TomcatServletWebServerFactory();
+    }
+
+    @Bean
+    public DispatcherServlet dispatcherServlet() {
+        return new DispatcherServlet();
+    }
+
     public static void main(String[] args) {
 
         AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
@@ -18,10 +30,13 @@ public class TobySpringApplication {
             protected void onRefresh() {
                 super.onRefresh();
 
-                TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+                ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
+                DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
+                dispatcherServlet.setApplicationContext(this);
+
                 WebServer webServer = serverFactory.getWebServer(servletContext -> {
                     servletContext.addServlet("dispatcherServlet",
-                            new DispatcherServlet(this)
+                            dispatcherServlet
                     ).addMapping("/*");
                 });
                 webServer.start();
