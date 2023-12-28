b901b9e75f1cce2b26047acc05714b65488ef862
Seokju Hong <iamseokjuhong@gmail.com>
2023-12-28 20:23:57 +0900

    DI를 이용한 Decorator, Proxy 패턴

---
+++ toby-spring/src/main/java/me/somefood/tobyspring/HelloDecorator.java
@@ -0,0 +1,21 @@
+package me.somefood.tobyspring;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+@Service
+@Primary
+public class HelloDecorator implements HelloService {
+
+    private final HelloService helloService;
+
+    // 자신을 제외한 빈 하나를 주입함
+    public HelloDecorator(HelloService helloService) {
+        this.helloService = helloService;
+    }
+
+    @Override
+    public String sayHello(String name) {
+        return "*" + helloService.sayHello(name) + "*";
+    }
+}
