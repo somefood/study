ecff8e11ce1f862e05dccd2a68d2aefb673ad61a
Seokju Hong <iamseokjuhong@gmail.com>
2023-12-30 23:43:53 +0900

    자동 구성 정보 대체하기

--- toby-spring/src/main/java/me/somefood/config/autoconfig/TomcatWebServerConfig.java
+++ toby-spring/src/main/java/me/somefood/config/autoconfig/TomcatWebServerConfig.java
@@ -2,6 +2,7 @@ package me.somefood.config.autoconfig;
 
 import me.somefood.config.ConditionalMyOnClass;
 import me.somefood.config.MyAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
@@ -10,6 +11,7 @@ import org.springframework.context.annotation.Bean;
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfig {
     @Bean("tomcatWebServerFactory")
+    @ConditionalOnMissingBean // 사용자 구성 정보가 먼저 찾아보고 없으면 등록해줌
     public ServletWebServerFactory servletWebServerFactory() {
         return new TomcatServletWebServerFactory();
     }
