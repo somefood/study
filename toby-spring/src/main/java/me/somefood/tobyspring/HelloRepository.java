1bc9e5255a2ae4789cd2af1fea45769da3be8de7
Seokju Hong <iamseokjuhong@gmail.com>
2024-01-01 23:36:41 +0900

    Hello 리포지토리
    자바 8의 default method와 static method 공부하려면
    Comparator 봐보기

---
+++ toby-spring/src/main/java/me/somefood/tobyspring/HelloRepository.java
@@ -0,0 +1,13 @@
+package me.somefood.tobyspring;
+
+public interface HelloRepository {
+
+    Hello findHello(String name);
+
+    void increase(String name);
+
+    default int countOf(String name) {
+        Hello hello = findHello(name);
+        return hello == null ? 0 : hello.getCount();
+    }
+}
